9495430603ce2a60c28973ab483c9e3c453ebf7a
Anna Udziela <annaudziela@gmail.com>
2020-08-05 14:54:36 +0200

    final commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,74 @@
 import '../scss/main.scss';
 
-// uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
 
-/* place your code below */
 
-console.log('HELLO 🚀')
+// const entry = localStorage.getItem('entry');
+// let result = '';
+
+// if (entry) {
+//     result = entry; 
+// };
+
+
+
+
+const textarea = document.querySelector('.textarea--js');
+const save = document.querySelector('.save--js');
+const load = document.querySelector('.load--js');
+
+const currentValue = localStorage.getItem('entry');
+
+if (currentValue) {
+    document.querySelector('.info--js').innerHTML = '💁🏼‍♀️';
+}
+
+save.addEventListener('click', (e) => {
+    e.preventDefault();
+   localStorage.setItem('entry', textarea.value);
+   if (textarea.value) {
+    document.querySelector('.info--js').innerHTML = '💁🏼‍♀️';
+} else {
+    document.querySelector('.info--js').innerHTML = '🤷🏼‍♀️';
+}
+})
+
+load.addEventListener('click', (e) => {
+    e.preventDefault();
+    textarea.value = localStorage.getItem('entry');
+   })
+
+
+
+
+
+
+// textArea.value = result;
+
+// const saveAnswer = document.querySelector('.button__save--action');
+// saveAnswer.addEventListener('click', () => {
+// localStorage.setItem('entry', textArea.value);
+// });
+
+// console.log(result);
+
+// // const entry = localStorage.getItem('entry');
+// // let result = '';
+
+// // if (entry) {
+// //     result = entry; 
+// // };
+
+// const previewAnswer = document.querySelector('.button__preview--action');
+// previewAnswer.addEventListener('click', () => {
+// localStorage.getItem(result);
+// });
+
+// console.log(previewAnswer);
+
+// // const saveAnswer = document.querySelector('.button__save--action');
+
+// console.log(textArea);
+
+
+
+// console.log('HELLO 🚀')
