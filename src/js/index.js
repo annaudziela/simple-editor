153bf567aac833816f37148f966cc026e083595e
Anna Udziela <annaudziela@gmail.com>
2020-08-10 12:59:21 +0200

    svg file manipulation

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,9 @@
 import '../scss/main.scss';
 
 
-
-// const entry = localStorage.getItem('entry');
-// let result = '';
-
-// if (entry) {
-//     result = entry; 
-// };
-
-
-
-
 const textarea = document.querySelector('.textarea--js');
 const save = document.querySelector('.save--js');
 const load = document.querySelector('.load--js');
-
 const currentValue = localStorage.getItem('entry');
 
 if (currentValue) {
@@ -25,6 +13,7 @@ if (currentValue) {
 save.addEventListener('click', (e) => {
     e.preventDefault();
    localStorage.setItem('entry', textarea.value);
+   
    if (textarea.value) {
     document.querySelector('.info--js').innerHTML = '💁🏼‍♀️';
 } else {
@@ -37,38 +26,5 @@ load.addEventListener('click', (e) => {
     textarea.value = localStorage.getItem('entry');
    })
 
-
-
-
-
-
-// textArea.value = result;
-
-// const saveAnswer = document.querySelector('.button__save--action');
-// saveAnswer.addEventListener('click', () => {
-// localStorage.setItem('entry', textArea.value);
-// });
-
-// console.log(result);
-
-// // const entry = localStorage.getItem('entry');
-// // let result = '';
-
-// // if (entry) {
-// //     result = entry; 
-// // };
-
-// const previewAnswer = document.querySelector('.button__preview--action');
-// previewAnswer.addEventListener('click', () => {
-// localStorage.getItem(result);
-// });
-
-// console.log(previewAnswer);
-
-// // const saveAnswer = document.querySelector('.button__save--action');
-
-// console.log(textArea);
-
-
-
+   
 // console.log('HELLO 🚀')
